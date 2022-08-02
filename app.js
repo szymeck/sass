e0a2ef992213c9197307e8b8dcca8d8639226d40
szymeck <szymek.sobierajski@gmail.com>
2022-08-02 16:40:31 +0200

    robota2

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-const section = document.querySelectorAll('.section');
+const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
-const secBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('main-content');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
 const pageTransition = function(){
     // button click active
-    for(let i=0;i<secBtn.length;i++){
-        secBtn[i].addEventListener('click',function(){
+    for(let i=0;i<sectBtn.length;i++){
+        sectBtn[i].addEventListener('click',function(){
             let currentBtn=document.querySelectorAll('.active-btn');
-            currentBtn[0].className=currentBtn[0].className.replace('active-btn','');
-            this.className+='active-btn'
+            currentBtn[0].className=currentBtn[0].className.replace('active-btn', '');
+            this.className+=' active-btn'
         })
     }
-}
+};
+
+
+
+// sections active
+allSections.addEventListener('click', (e) =>{
+    const id = e.target.dataset.id;
+    if(id){
+        // // remove selected from other btns
+        // sectBtns.forEach((btn)=>{
+        //     btn.classList.remove('active')
+        // })
+        // e.target.classList.add('active')
+        // // hide other sections
+        sections.forEach((section)=>{
+            section.classList.remove('active')
+        })
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+});
 pageTransition();
